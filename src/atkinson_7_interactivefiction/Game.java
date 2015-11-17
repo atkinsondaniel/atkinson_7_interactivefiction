f7eb196c0ee6cdfe608b74d7985b921f965a8184
Daniel Atkinson <atkinsondaniel123@gmail.com>
2015-11-17 15:48:22 -0600

    "1117_1"

--- src/atkinson_7_interactivefiction/Game.java
+++ src/atkinson_7_interactivefiction/Game.java
@@ -7,135 +7,117 @@ package atkinson_7_interactivefiction;
 
 import java.util.Scanner;
 
-
-
 /**
  *
  * @author danielatkinson
  */
 public class Game {
+
     Scanner scanner = new Scanner(System.in); //an instance of the scanner class, wow!
     Atkinson_7_interactivefiction thing = new Atkinson_7_interactivefiction(); //an instance of my main class, wow!
+
     int check(String usrChoice, String[] stuff) { //checks users response and assigns them a number
         int response; //creates the response variable
-        if (usrChoice.contains(stuff[1])) { 
-            response = 1; 
-        }
-        else if (usrChoice.contains(stuff[2])) {
+        if (usrChoice.contains(stuff[1])) {
+            response = 1;
+        } else if (usrChoice.contains(stuff[2])) {
             response = 2;
-        }
-        else if (usrChoice.contains(stuff[3])) {
+        } else if (usrChoice.contains(stuff[3])) {
             response = 3;
-        }
-        else { //does nothing, haven't bothered to delete it yet
+        } else { //does nothing, haven't bothered to delete it yet
             response = 4;
         }
         return response;
     }
+
     void text(String[] stuff) {
         System.out.println(stuff[0]); //prints scenario
         String response = scanner.next().toLowerCase(); //gets response
-        int ans = check(response,stuff); //get value of the response
+        int ans = check(response, stuff); //get value of the response
         setPath(ans); //decides where to go next
     }
+
     void setPath(int res) {
-        switch(thing.path) { //gigantic and bad, didn't feel like doing anything else
+        switch (thing.path) { //gigantic and bad, didn't feel like doing anything else
             case 1: //start
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 2; //more
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     thing.path = 3; //less
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
             case 2: //more
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 4; //house
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     thing.path = 5; //barn
-                }
-                else if(res == 3) {
+                } else if (res == 3) {
                     thing.path = 1; //keep
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
             case 3: //less
                 thing.path = 16; //start
             case 4: //house
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 2; //leave
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     thing.path = 6; //closet
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
             case 5: //barn
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 7; //duck
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     thing.path = 8; //dive
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
             case 6: //closet
-                thing.path = 16; //start
+                thing.path = 1; //start
             case 7: //duck
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 16; //keep
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     thing.path = 9; //forest
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
             case 8: //dive
                 thing.path = 16; //start
             case 9: //forest
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 10; //deeper
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     thing.path = 11; //swim
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
             case 10: //deeper
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 12; //enter
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     thing.path = 13; //stay
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
             case 11: //swim
-                thing.path = 16; 
+                thing.path = 16;
             case 12: //enter
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 14; //search
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     thing.path = 15; //give up
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
@@ -144,19 +126,23 @@ public class Game {
                 break;
             case 14: //search
                 thing.live = false;
+                System.out.println("It only took you " + (System.currentTimeMillis() - thing.sTime) / 1000 + " seconds"
+                        + " to die. Impressive");
                 break;
             case 15: //give up
                 thing.live = false;
+                System.out.println("I only knew you for " + (System.currentTimeMillis() - thing.sTime) / 1000 + " seconds"
+                        + ". Why must the good die young.");
                 break;
             case 16:
-                if (res == 1){
+                if (res == 1) {
                     thing.path = 1;
-                }
-                else if(res == 2) {
+                } else if (res == 2) {
                     System.out.println("Pitiful");
+                    System.out.println("It only took you " + (System.currentTimeMillis() - thing.sTime) / 1000 + " seconds"
+                            + " to give up");
                     thing.live = false;
-                }
-                else {
+                } else {
                     System.out.println("Learn how to spell");
                 }
                 break;
